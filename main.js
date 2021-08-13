d475346c50e0ce16581561e4b88069839ce499cd
Rick-neitzen <76189555+Rick-neitzen@users.noreply.github.com>
2021-08-13 15:30:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,6 +22,53 @@ function modelLoaded() {
     console.log('Model Loaded!');
 }
 
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Peace Sign")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#270C;";
+        }
+
+        if(results[0].label == "OK Sign")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+
+        if(results[0].label == "Finger Gun")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128073;";
+        }
+
+        if(results[1].label == "Peace Sign")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#270C;";
+        }
+
+        if(results[1].label == "OK Sign")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+
+        if(results[1].label == "Finger Gun")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128073;";
+        }
+    }
+}
+
 function speak(){
     var synth = window.speechSynthesis;
     speak_data_1 = "the first prediction is " + prediction_1;
